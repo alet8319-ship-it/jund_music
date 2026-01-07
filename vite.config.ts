ed73d04c964582c2ccaf254d222e8c17b4e9fef7
JundAlNabi <alet8319@gmail.com>
2026-01-08 01:01:52 +0500

    Remove unnecessary comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jund_music/', // <--- important for GitHub Pages
+  base: '/jund_music/',
   server: {
     port: 5173,
   },
